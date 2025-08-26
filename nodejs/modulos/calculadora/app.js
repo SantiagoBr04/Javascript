2a81d85dac7221beb1ea30549866b0424b969c8a
Santiago de Quadros <santiagoquadros9@gmail.com>
2025-08-26 11:05:48 -0300

    versão calculadora

---
+++ nodejs/modulos/calculadora/app.js
@@ -0,0 +1,17 @@
+const funcoesMatematicas = require('./funcoes_matematicas');
+
+// Exemplo de uso:
+const resultadoSoma = funcoesMatematicas.somar(5, 3);
+console.log(`Resultado da soma: ${resultadoSoma}`);
+
+const resultadoSubtracao = funcoesMatematicas.subtrair(5, 3);
+console.log(`Resultado da subtração: ${resultadoSubtracao}`);
+
+const resultadoMultiplicacao = funcoesMatematicas.multiplicar(5, 3);
+console.log(`Resultado da multiplicação: ${resultadoMultiplicacao}`);
+
+const resultadoDivisao = funcoesMatematicas.dividir(5, 3);
+console.log(`Resultado da divisão: ${resultadoDivisao}`);
+
+const resultadoIdade = funcoesMatematicas.idade(2008);
+console.log(`Idade em 2025: ${resultadoIdade}`);    
