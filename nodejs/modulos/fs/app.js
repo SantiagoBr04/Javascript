b3dba7dfe2db0a55d65a626284d47dfbc3843f3a
Santiago de Quadros <santiagoquadros9@gmail.com>
2025-09-02 10:48:34 -0300

    Modulos comentados

--- nodejs/modulos/fs/app.js
+++ nodejs/modulos/fs/app.js
@@ -1,11 +1,19 @@
+// Importa o modulo fs
 const fs = require('fs');
+
+// Lê o conteúdo de um arquivo de forma assíncrona
 fs.readFile('example.txt', 'utf8', (err, data) => {
-    if (err) {
+    // Trata erros e exibe o conteúdo do arquivo
+    if (err) { 
+        // Exibe mensagem de erro no console
         console.error('Erro ao ler o arquivo', err);
+        // Retorna para evitar execução adicional
         return;
     }
+    // Exibe o conteúdo do arquivo no console
     console.log('Conteúdo do arquivo:', data);
 });
 
+// Exibe uma mensagem imediatamente, mostrando a natureza assíncrona do readFile
 console.log('Esta mensagem aparce primeiro');
 
--- nodejs/modulos/http/app.js
+++ nodejs/modulos/http/app.js
@@ -1,5 +1,10 @@
+// Importa o modulo http
 const http = require('http');
+// Cria o server
 http.createServer((req, res) => {
+  // Define o cabeçalho e o corpo da resposta
   res.writeHead(200, {'Content-Type': 'text/plain'});
+  // Envia a resposta "Hello World!"
   res.end('Hello World!');
+  // Escuta na porta 8080
 }).listen(8080);
