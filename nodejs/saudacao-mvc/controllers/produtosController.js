9ae741d078df27885220ff9a5896ff4f2a1a1488
Santiago de Quadros <santiagoquadros9@gmail.com>
2025-10-28 10:19:58 -0300

    Cadastro de produtos

--- nodejs/saudacao-mvc/controllers/produtosController.js
+++ nodejs/saudacao-mvc/controllers/produtosController.js
@@ -1,3 +1,5 @@
+const cadastrar = require("../models/cadastrar");
+
 module.exports = {
 
     produtos: (req, res) => {
@@ -6,5 +8,15 @@ module.exports = {
 
     camisetas: (req, res) => {
         res.sendfile('camisetas.html', { root: 'views' });
+    },
+
+    cadastrar: (req, res) => {
+        res.sendfile('cadastrar.html', { root: 'views' });
+    },
+
+    cadastrarProduto: (req, res) => {
+        const { id, descricao, quantidade, preco} = req.body;
+        const mensagemProdutos = cadastrar.cadastroMensagem(id, descricao, quantidade, preco);
+        res.send(`<h1>${mensagemProdutos}</h1>`);
     }
 };
--- nodejs/saudacao-mvc/routes/produtos.js
+++ nodejs/saudacao-mvc/routes/produtos.js
@@ -5,5 +5,7 @@ var produtosController = require('../controllers/produtosController');
 /* GET users listing. */
 router.get("/",produtosController.produtos);
 router.get("/camisetas", produtosController.camisetas);
+router.get("/cadastrar", produtosController.cadastrar);
+router.post("/cadastrar", produtosController.cadastrarProduto);
 
 module.exports = router;
